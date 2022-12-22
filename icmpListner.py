eb3c3f09f765e62660517e05bac0e44ee2169abe
siguritech <77058644+siguritech@users.noreply.github.com>
2022-12-22 22:30:07 +0100

    Create icmpListner.py

---
+++ icmpListner.py
@@ -0,0 +1,11 @@
+import socket
+
+def listen():
+  s = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_ICMP)
+  s.setsockopt(socket.SOL_IP, socket.IP_HDRINCL, 1)
+  while 1:
+    data, addr = s.recvfrom(1024)
+    print("Packet from %r: %r" % addr,data)
+
+
+listen()
